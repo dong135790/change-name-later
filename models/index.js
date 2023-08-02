88a17e3bfc7cb14d680083b51036f6b29124a8b9
Justin <dong135790@gmail.com>
2023-08-01 21:43:34 -0400

    model index adjustment

--- models/index.js
+++ models/index.js
@@ -1,7 +1,4 @@
 // Require the modules from same directory (folder)
-const User = require('./Excercise');
-const User = require('./User');
-
 const Exercise = require('./Exercise');
 const User = require('./User');
 
--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
-
-app.listen(PORT, () => {
-  console.log(`Listening at http://localhost:${PORT}`)
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Listening at http://localhost:${PORT}`)
+// });
