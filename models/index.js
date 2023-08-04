1cb1cfd772113c335b31d2aed903e12e369edc8f
Justin <dong135790@gmail.com>
2023-08-03 20:49:23 -0400

    added routine module and connected to exercise

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 // Require the modules from same directory (folder)
 const Exercise = require('./Exercise');
 const User = require('./User');
+const Routine = require('./Routine');
 
+Routine.hasMany(Exercise, {
+    foreignKey: 'routine_id',
+  });
+  
+  Exercise.belongsTo(Routine, {
+    foreignKey: 'routine_id',
+  });
 
-
-module.exports = { Exercise, User }
+module.exports = { Exercise, User, Routine }
