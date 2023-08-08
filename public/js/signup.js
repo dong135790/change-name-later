fc4309abc25585dd4b2e2786fe1449b894b52159
Justin <dong135790@gmail.com>
2023-08-07 20:34:42 -0400

    finished signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,12 @@
 const signupFormHandler = async (event) => {
+    console.log('hi')
     event.preventDefault();
+    console.log('hi')
   
     const username = document.querySelector('#username').value.trim();
     const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password').value.trim();
+    console.log("hi",email)
   
     if (username && email && password) {
       const response = await fetch('/api/users', {
@@ -11,10 +14,14 @@ const signupFormHandler = async (event) => {
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
+      console.log("hi",email)
   
       if (response.ok) {
         document.location.replace('/');
       } else {
+        console.log(password)
+        console.log("hi",email)
+
         alert('Failed to sign up.');
       }
     }
